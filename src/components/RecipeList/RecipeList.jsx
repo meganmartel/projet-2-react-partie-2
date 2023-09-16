1558c92e7e6381e5d47c17217854802717ab0a08
Megan Martel <115752040+meganmartel@users.noreply.github.com>
2023-09-16 09:28:27 -0400

    import the store to the main

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
-import MealService from '../../RecipeList/service/MealService';
+import MealService from '../../service/MealService';
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {Provider} from 'react-redux'; 
+import store from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
